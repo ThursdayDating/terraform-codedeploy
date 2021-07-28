dc1cecabeba8ae1663101960bf9c3c58826a28ae
Josh <josh@getthursday.com>
2021-07-28 14:40:18 +0100

    Updated deployment config name

--- deployment-group/main.tf
+++ deployment-group/main.tf
@@ -3,6 +3,7 @@ resource "aws_codedeploy_deployment_group" "deployment_group" {
   deployment_group_name = "${var.app_name}-${var.environment}"
   service_role_arn      = var.service_role_arn
   autoscaling_groups    = var.autoscaling_groups
+  deployment_config_name = "CodeDeployDefault.AllAtOnce"
 
   auto_rollback_configuration {
     enabled = var.rollback_enabled
